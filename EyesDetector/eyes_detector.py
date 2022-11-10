a0174f1021d8fcaaaebe49530bebe2eac563fcb3
yanngan <55285071+yanngan@users.noreply.github.com>
2022-11-10 18:55:55 +0200

    init an virtual env +firs eyes detection in img
    1. try to init an virtual env  (?)
    2. firs try to detect eyes in img

--- EyesDetector/eyes_detector.py
+++ EyesDetector/eyes_detector.py
@@ -2,9 +2,23 @@ import cv2
 
 imgNames = ('center.jpg', 'down.jpg', 'left.jpg','mid.jpg', 'right.jpg', 'up.jpg')
 
+base_path = "../Assets/"
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
 imgs = []
 for imgName in imgNames:
-    imgs.append(cv2.imread(imgName))
+    imgs.append(cv2.imread(base_path + imgName))
+
+for i in range(0, 5):
+    eyes = eye_cascade.detectMultiScale(imgs[i], scaleFactor=1.2)
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(imgs[i], (x, y), (x + w, y + h), (0, 255, 0), 5)
+        cv2.imshow("Eyes Detected", imgs[i])
 
 
-print(imgs)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
